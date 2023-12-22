b6f3ed7c61ae272867ffa5c219197c29006250f0
Ninad Vyas <115214615+NinadVyas@users.noreply.github.com>
2023-12-22 22:14:16 +0530

    made changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,48 @@
+"use client"
 import { title } from "@/components/primitives";
+import React from "react";
+import {Card, CardHeader, CardBody, CardFooter, Avatar, Button} from "@nextui-org/react";
 
 export default function AboutPage() {
+	const [isFollowed, setIsFollowed] = React.useState(false);
+
 	return (
 		<div>
-			<h1 className={title()}>About</h1>
+			<h1 className="text-6xl	font-extrabold	text-emerald-500">About</h1>
+			<Card className="max-w-[340px] mt-3">
+      <CardHeader className="justify-between">
+        <div className="flex gap-5">
+          <Avatar isBordered radius="full" size="md" src="/ninad.jpeg" />
+          <div className="flex flex-col gap-1 items-start justify-center">
+            <h4 className="text-small font-semibold leading-none text-default-600">Ninad Vyas</h4>
+            <a href="https://ninadvyas.tech" target='_blank'><h5 className="text-small tracking-tight text-default-400">@ninadvsd</h5></a>
+          </div>
+        </div>
+        <Button
+          className={isFollowed ? "bg-transparent text-foreground border-default-200" : ""}
+          color="primary"
+          radius="full"
+          size="sm"
+          variant={isFollowed ? "bordered" : "solid"}
+          onPress={() => setIsFollowed(!isFollowed)}
+        >
+          {isFollowed ? "Unfollow" : "Follow"}
+        </Button>
+      </CardHeader>
+      <CardBody className="px-3 py-0 text-small text-default-400">
+        <p>
+       		 Developer and UI/UX enthusiast. Join me on this coding adventure!
+        </p>
+        <span className="pt-2">
+          #cookie
+          <span className="py-2" aria-label="computer" role="img">
+            
+          </span>
+        </span>
+      </CardBody>
+      <CardFooter className="gap-3">
+      </CardFooter>
+    </Card>
 		</div>
 	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,15 @@ import { Snippet } from "@nextui-org/snippet";
 import { Code } from "@nextui-org/code"
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 
 export default function Home() {
 	return (
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>All In One</h1>
+			<div className="inline-block text-center justify-center">
+			<h1 className="text-9xl	font-extrabold	text-emerald-500	">Cook,Share,Explore </h1>
 				<br />
-				<h1 className={title({ color: "violet" })}>Recipe Finder</h1>
+				<h1 className="text-9xl	font-extrabold	text-emerald-500">Your Recipes Wanted!</h1>
 			</div>
 
 			<div className="flex gap-3">
